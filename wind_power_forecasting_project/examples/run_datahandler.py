24d9424886a348030eb745450b36348784cbc637
ApostolosDTU <s242900@dtu.dk>
2025-04-09 18:33:58 +0300

    Initial commit of wind power forecasting project

--- wind_power_forecasting_project/examples/run_datahandler.py
+++ wind_power_forecasting_project/examples/run_datahandler.py
@@ -7,9 +7,10 @@ retrieving command-line arguments, and handling standard input/output streams.
 # Provides access to Python runtime environment, including the module search path (sys.path)
 import os
 # Add the 'inputs' folder to Python's module search path
-from inputs.datahandler import DataHandler
+from inputs.datahandler import DataHandler # Imports the DataHandler class from the datahandler.py file inside the inputs folder.
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'inputs'))
+# Appends the 'inputs' directory to the system path so Python can find and import modules from it.
 
-if __name__ == "__main__":
-    handler = DataHandler()
-    handler.plot_variable("Location1", "Power", 0, 200)
+if __name__ == "__main__": # Checks if the script is being run directly (not imported
+    handler = DataHandler() # Creates an instance of the DataHandler class, which loads CSV files from the 'inputs' folder.
+    handler.plot_variable("Location1", "Power", 0, 200) # Calls the plot_variable method to plot the "Power" variable from "Location1" CSV file, slicing the first 200 rows.
